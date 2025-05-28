b1e5c2d6137504642c89bfe9ffc62022080b9aa6
Sam Schonenberg <sam.schonenberg@gmail.com>
2025-05-28 14:06:34 +0200

    updated public folder

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,9 +12,11 @@ const About = () => {
             <div className="md:flex-shrink-0">
               {/* Placeholder for profile image */}
               <div className="h-48 w-full md:w-48 bg-gray-200 flex items-center justify-center">
-                <svg className="h-24 w-24 text-gray-400" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                </svg>
+                <img 
+                  src="/images/profile.jpg" 
+                  alt="Profile picture" 
+                  className="h-24 w-24 rounded-full object-cover"
+                />
               </div>
             </div>
             <div className="p-8">
